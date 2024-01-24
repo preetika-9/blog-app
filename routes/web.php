bf6d9d40c65fdbc2f0e524d7dfd3d2d04b281052
Pritika-01 <preetika@ambition.guru>
2024-01-24 14:17:59 +0545

    setup blade, routes, navigation

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// using closure
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// using controller
+// welcome page
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
+
+// blog page
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+
+// to single blog post
+Route::get('/blog/post', [BlogController::class, 'show'])->name('blog.show');
+
+
+// to about page
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// to contact page
+Route::get('/contact-us', [ContactController::class, 'index'])->name('contact.index');

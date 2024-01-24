2931a3fdebcec36b971b2ef11a522af5558adce4
Pritika-01 <preetika@ambition.guru>
2024-01-24 14:38:59 +0545

    authentication login n register added

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,11 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 
-
-// using closure
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 // using controller
 // welcome page
@@ -22,11 +18,23 @@ Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 // to single blog post
 Route::get('/blog/post', [BlogController::class, 'show'])->name('blog.show');
 
-
-// to about page
+// using closure
+// to about page 
 Route::get('/about', function () {
     return view('about');
 })->name('about');
 
 // to contact page
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';

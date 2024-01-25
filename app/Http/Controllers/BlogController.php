7ba0aec273cd2ff1373f798d0d1c180b9655f472
Pritika-01 <preetika@ambition.guru>
2024-01-25 11:40:05 +0545

    middleware

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,7 +11,7 @@ class BlogController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth')->except(['index', 'show']);
     }
     public function index()
     {
